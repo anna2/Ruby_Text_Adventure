43e6e02f574f3426dcbb70cd0d0c97456ff6f6ed
Anna Sheaffer <annasheaffer@gmail.com>
2013-09-30 09:53:18 -0400

    update room and item inventory

--- game.rb
+++ game.rb
@@ -1,38 +1,18 @@
-class Grandmas_House
-  attr_accessor :player
-  
-  def initialize(player_name)
-    @player = Player.new(player_name)
+class House
+  attr_accessor  :rooms, :items, :player
+    
+  def initialize(player)
+    @player = player
     @rooms=[]
     @items=[]
-    @sanity=100
   end
   
   def create_room(tag, description, links, inventory)
     @rooms << Room.new(tag, description, links, inventory)
   end
   
-  def start(location) #Customize starting room in Grandma's House; location should be enterred in the form of a room's tag
-      @player.location = location
-  end  
-  
-  def find_room(tag) #Search through rooms array to find a room by its tag
+  def find_room(tag)
       @rooms.detect {|room| room.tag == tag}
-  end
-  
-  def get_room_in_direction(direction) #Given a direction, update players's current location by finding that direction key in the current location's links hash. Print description of new room.
-      @player.location = find_room(@player.location).links[direction]
-      puts find_room(@player.location).room_description
-  end
-  
-  def move(direction)
-      puts "You move " + direction.to_s
-      get_room_in_direction(direction)
-      if find_room(@player.location).inventory == nil
-          puts "No items in this room."
-      else
-          puts "Items in this room: " + find_room(@player.location).inventory.to_s
-      end
   end
   
   def create_item(tag, name, description, sanity_points, room)
@@ -43,70 +23,146 @@ class Grandmas_House
       @items.detect {|item| item.tag == tag}
   end
   
+  def prompt_action
+    puts 'What would you like to do? ("go, "investigate")'
+    command = gets.chomp
+    if command == "go"
+      puts "In which direction?"
+      dir = gets.chomp
+      player.move(dir)
+    elsif command == "investigate"
+      puts "What object would you like to investigate?"
+      obj = gets.chomp
+      player.investiage(obj)
+    else
+      puts "That's not allowed in Grandma's house! Try something else."
+    end
+  end
+end
+
+
+
+class Player
+  attr_accessor :name, :location, :sanity
+
+  def initialize(name)
+      @name = name
+      @location = :kitchen
+      @sanity = 100
+  end
+  
+  #Update players's current location by finding the direction key in the current location's links hash. Print description of new room. Print inventory list.
+  def move(direction)
+      puts "You move " + direction.to_s
+      @location = find_room(@location).links[direction]
+      puts find_room(@location).room_description
+      if find_room(@location).inventory == nil
+          puts "No items in this room."
+      else
+          puts "Items in this room: " + find_room(@location).inventory.to_s
+      end
+  end
+  
+  #Describe items. Print how item affects sanity.
   def investigate(tag)
     puts find_item(tag).item_description
     @sanity = @sanity + find_item(tag).sanity_points
     puts @sanity
   end
-    
-    class Player
-      attr_accessor :name, :location
-  
-      def initialize(name)
-          @name = name
-	    end
-  	end
-
-    class Room
-      attr_accessor :tag, :description, :links, :inventory
-      
-      def initialize(tag, description, links, inventory)
-        @tag = tag
-        @description = description
-        @links = links
-        @inventory = inventory
-      end
-      
-      def room_description
-        "You are in " + @description + "."
-      end
-    end
-    
-    class Item
-      attr_accessor :tag, :name, :description, :sanity_points, :room
-          
-        def initialize(tag, name, description, sanity_points, room)
-          @tag = tag
-          @name = name
-          @description = description
-          @sanity_points = sanity_points
-          @room = room
-        end
-        
-        def item_description
-            @description
-        end
-        
-    end  
 end
 
-#Create a new game and player.
-new_game = Grandmas_House.new("Terry")
+
+
+class Room
+  attr_accessor :tag, :description, :links, :inventory
+  
+  def initialize(tag, description, links, inventory)
+    @tag = tag
+    @description = description
+    @links = links
+    @inventory = inventory
+  end
+  
+  def room_description
+    "You are in " + @description + "."
+  end
+end
+
+
+
+class Item
+  attr_accessor :tag, :name, :description, :sanity_points, :room
+      
+    def initialize(tag, name, description, sanity_points, room)
+      @tag = tag
+      @name = name
+      @description = description
+      @sanity_points = sanity_points
+      @room = room
+    end
+    
+    def item_description
+        @description
+    end
+    
+end  
+
+
+
+
+#Let the game begin!
+puts "Welcome to Grandma's House: The Text Adventure!"
+puts "Can you clear up the mystery around Grandma's whereabouts before your sanity reserves are depleted?"
+puts "To begin, what's your name?"
+
+#get player name
+name = nil
+while name.nil?
+  print "Player name: "
+  name = gets.chomp
+end
+
+#create new house and player
+player = Player.new(name)
+house = House.new(player)
 
 #Create a few rooms inside Grandma's House.
-new_game.create_room(:kitchen, "an avocado-green kitchen full of fruit flies", {:west => :parlor, :north => :basement}, nil)
-new_game.create_room(:parlor, "a parlor stacked to the ceiling with National Geographic magazines", {:east => :kitchen}, nil)
-new_game.create_room(:basement, "a basement full of canning jars and 1940s exercise machines. To the south you can see the stairs you just fell down and the light from the kitchen", {:south => :kitchen}, :pickles)
+house.create_room(:kitchen, "an avocado-green kitchen full of fruit flies", {:west => :parlor, :north => :basement}, nil)
+house.create_room(:parlor, "a parlor stacked to the ceiling with National Geographic magazines", {:east => :kitchen}, nil)
+house.create_room(:basement, "a basement full of canning jars and 1940s exercise machines. To the south you can see the stairs you just fell down and the light from the kitchen", {:south => :kitchen}, :pickles)
+=begin
+house.create_room(:front_yard)
+house.create_room(:back_yard)
+house.create_room(:bedroom)
+house.create_room(:shed)
+=end
 
 #Create some objects
-new_game.create_item(:pickles, "Pickle Jar", "Ugh! That pickle was gross. In fact, you can feel a bout of botulism-induced insanity taking hold.", -10, :basement)   
+house.create_item(:pickles, "Pickle Jar", "Ugh! That pickle was gross. In fact, you can feel a bout of botulism-induced insanity taking hold.", -10, :basement) 
+=begin
+house.create_item(:shovel, )
+house.create_item(:fragment1, )
+house.create_item(:fragment2, )
+house.create_item(:fragment3, )
+house.create_item(:key, )
+house.create_item(:armoire, )
+house.create_item(:catnip, )
+house.create_item(:phone, )
+=end
 
-#Begin interaction with player.
-puts "Welcome to Grandma's House: The Text Adventure! Can you clear up the mystery around Grandma's whereabouts before your sanity reserves are depleted? Right now, you're standing in the kitchen where there's a weird smell. What would you like to do?"
-new_game.start(:kitchen)
 
-#Sample choices made by player.
-new_game.move(:west)
-new_game.move(:east)
-new_game.move(:north)
-new_game.investigate(:pickles)
+#start game play
+puts "Welcome, #{name}. You're in the kitchen where there's a weird smell."
+game_over = false
+while game_over == false
+  house.prompt_action
+end
+
+if @sanity <=50
+  puts "Your mind is disintegrating at an alrming rate. Sanity level: at or below 50%! Careful now!"
+elsif @sanity <=10
+  puts "Your sanity level is at or below 10%! Better pause to get a grip on reality!"
+elsif @sanity <=0
+  puts "Like many an adventurer before you, Grandma's House has driven you mad! GAME OVER."
+  game_over = true
+end
