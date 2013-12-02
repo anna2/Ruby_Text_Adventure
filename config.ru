95f5a81d97b0dadd9307a88268cadc19ac22cb11
Anna Sheaffer <annasheaffer@gmail.com>
2013-12-01 20:54:21 -0500

    Remove cached files

--- config.ru
+++
@@ -1,8 +0,0 @@
-$:.unshift File.expand_path("./../lib", __FILE__)
-
-require 'bundler'
-Bundler.require
-
-require 'controller'
-
-run TextGame
--- lib/files.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'house/house.rb'
-require_relative 'house/satchel.rb'
-require_relative 'house/monster.rb'
-require_relative 'house/item.rb'
-require_relative 'house/room.rb'
-require_relative 'house/player.rb'
-require_relative 'house/game.rb'
-require_relative 'house/headlines.rb'
